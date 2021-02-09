ed25011b2f191f57a0912a860177c2255de8e403
Kolmagin Danil <dkolmagin@gmail.com>
2021-02-09 16:04:33 +0200

    Add testing work with CSV

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('First commit');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
+
+const path = 'test.csv'
+
+const header = [
+    {id: 'name', title: 'Name'},
+    {id: 'surname', title: 'Surname'},
+    {id: 'age', title: 'Age'},
+    {id: 'gender', title: 'Gender'},
+]
+
+const data = [
+    {
+        name: 'John',
+        surname: 'Snow',
+        age: 26,
+        gender: 'M',
+    }, {
+        name: 'Clair',
+        surname: 'White',
+        age: 33,
+        gender: 'F',
+    }, {
+        name: 'Fancy',
+        surname: 'Brown',
+        age: 78,
+        gender: 'F',
+    }
+]
+
+createCsvWriter({path, header}).writeRecords(data)
+    .then(() => console.log('The CSV file was written successfully'))
